17847a95ab2bcf8232600a2d0465da4c3409b25d
kenta-s <knt01222@gmail.com>
2019-10-12 17:45:10 +0900

    add assets false to application.rb

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,8 @@ module Alder
     # Don't generate system test files.
     config.generators.system_tests = nil
     config.generators do |g|
-      g.javascripts false
       g.helper false
+      g.assets false
     end
   end
 end
