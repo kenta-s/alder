c4be67feb5f1821fe35e11bcddfcb6912b523ab7
kenta-s <knt01222@gmail.com>
2019-10-13 19:08:39 +0900

    add tasks_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :tasks, only: [:index, :show]
+
   namespace :api, { format: 'json' } do
     namespace :v1 do
       resources :tasks, only: [:index, :show]
