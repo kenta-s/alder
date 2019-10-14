519be14099d31572ac9e469b1a1a0ddd62342689
kenta-s <knt01222@gmail.com>
2019-10-14 19:49:55 +0900

    remove unused comment

--- app/javascript/packs/components/Tasks.js
+++ app/javascript/packs/components/Tasks.js
@@ -1,56 +1,3 @@
-// import React from "react";
-// import { connect } from "react-redux"
-// import { withRouter } from 'react-router'
-// import {
-//   fetchTasks,
-// } from "../redux/actions/tasks"
-// 
-// class Tasks extends React.Component {
-//   componentDidMount() {
-//     this.props.fetchTasks()
-//   }
-//   render(){
-// 	  const tasks = this.props.tasks.map(task => {
-// 		  return(
-//         <tr key={task.id} style={{cursor: 'pointer'}} onClick={() => this.props.history.push(`/tasks/${task.id}`)}>
-//           <td>{task.title}</td>
-//           <td>{task.end_at}</td>
-//         </tr>
-//       )
-// 		})
-//     return(
-//       <table className="highlight">
-//         <thead>
-//           <tr>
-//               <th>Title</th>
-//               <th>Expires at</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {tasks}
-//         </tbody>
-//       </table>
-//     )
-//   }
-// }
-// 
-// const mapStateToProps = state => {
-//   return { 
-//     tasks: state.tasks.data,
-//   }
-// };
-// 
-// const mapDispatchToProps = dispatch => {
-//   return {
-//     fetchTasks: () => dispatch(fetchTasks()),
-//   }
-// }
-// 
-// export default withRouter(connect(
-//   mapStateToProps,
-//   mapDispatchToProps 
-// )(Tasks));
-
 import React from "react";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -69,14 +16,6 @@ class Tasks extends React.Component {
     this.props.fetchTasks()
   }
   render(){
-	  // const tasks = this.props.tasks.map(task => {
-		//   return(
-    //     <tr key={task.id} style={{cursor: 'pointer'}} onClick={() => this.props.history.push(`/tasks/${task.id}`)}>
-    //       <td>{task.title}</td>
-    //       <td>{task.end_at}</td>
-    //     </tr>
-    //   )
-		// })
     const tasks = this.props.tasks
     return(
       <React.Fragment>
