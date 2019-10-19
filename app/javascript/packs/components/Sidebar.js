605668168bce8ff6660f4d836fffbd42102450b5
kenta-s <knt01222@gmail.com>
2019-10-20 00:23:53 +0900

    update

--- app/javascript/packs/components/Sidebar.js
+++ app/javascript/packs/components/Sidebar.js
@@ -1,31 +1,31 @@
-import React from "react";
-import { connect } from "react-redux"
-import { withRouter } from 'react-router'
-// import {SideNav, SideNavItem, Button} from 'react-materialize'
-
-class Sidebar extends React.Component {
-  render(){
-    return(
-        <div className="collection sidenav sidenav-fixed" style={{marginTop: '67px'}}>
-          <a href="#!" className="collection-item" onClick={() => this.props.history.push(`/tasks`)} >
-            募集中のタスク
-          </a>
-        </div>
-    )
-  }
-}
-
-const mapStateToProps = state => {
-  return { 
-  }
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-  }
-}
-
-export default withRouter(connect(
-  mapStateToProps,
-  mapDispatchToProps 
-)(Sidebar))
+// import React from "react";
+// import { connect } from "react-redux"
+// import { withRouter } from 'react-router'
+// // import {SideNav, SideNavItem, Button} from 'react-materialize'
+// 
+// class Sidebar extends React.Component {
+//   render(){
+//     return(
+//         <div className="collection sidenav sidenav-fixed" style={{marginTop: '67px'}}>
+//           <a href="#!" className="collection-item" onClick={() => this.props.history.push(`/tasks`)} >
+//             募集中のタスク
+//           </a>
+//         </div>
+//     )
+//   }
+// }
+// 
+// const mapStateToProps = state => {
+//   return { 
+//   }
+// };
+// 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//   }
+// }
+// 
+// export default withRouter(connect(
+//   mapStateToProps,
+//   mapDispatchToProps 
+// )(Sidebar))
