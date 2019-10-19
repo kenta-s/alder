9228c19ea1ddd008be4cbb6b25ef32017b48abf8
kenta-s <knt01222@gmail.com>
2019-10-20 07:53:36 +0900

    fix

--- app/javascript/packs/components/MessageCounts.js
+++ app/javascript/packs/components/MessageCounts.js
@@ -25,18 +25,6 @@ const useStyles = makeStyles({
   }
 });
 
-// function createData(name, unreadCount) {
-//   return { name, unreadCount };
-// }
-// 
-// const rows = [
-//   createData('Frozen yoghurt', 0),
-//   createData('Ice cream sandwich', 0),
-//   createData('Eclair', 0),
-//   createData('Cupcake', 0),
-//   createData('Gingerbread', 0),
-// ];
-
 function MessageCounts({messageCounts, fetchMessageCounts, history}) {
   const classes = useStyles();
   useEffect(() => {
@@ -48,24 +36,31 @@ function MessageCounts({messageCounts, fetchMessageCounts, history}) {
   return (
     <React.Fragment>
       <Title>Messages</Title>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>name</TableCell>
-            <TableCell align="right">unread</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map(row => (
-            <TableRow key={row.name} className={classes.row} onClick={() => { history.push(`/users/${row.name}/messages`) }}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.unreadCount}</TableCell>
+      { rows.length > 0
+        ?
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>name</TableCell>
+              <TableCell align="right">unread</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+          </TableHead>
+          <TableBody>
+            {rows.map(row => (
+              <TableRow key={row.name} className={classes.row} onClick={() => { history.push(`/users/${row.name}/messages`) }}>
+                <TableCell component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="right">{row.unreadCount}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        :
+        <div>
+          メッセージはありません
+        </div>
+      }
     </React.Fragment>
   );
 }
