6bbeea94e05f63c3ec60b7a44d18bd26f63185ed
kenta-s <knt01222@gmail.com>
2019-10-21 00:18:06 +0900

    modify mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,4 @@
+class UserMailer < Devise::Mailer   
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+end
