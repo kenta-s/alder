492877111f6ecfb29b857ef42b3ae9d0acbd4630
kenta-s <knt01222@gmail.com>
2019-10-20 19:13:33 +0900

    mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,14 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'https://www.proprogramming.net' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'www.proprogramming.net',
+    :authentication => :plain,
+  }
+
 end
